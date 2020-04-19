a4b92b7f29d66522c30a88ef399006f19cc399db
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-04-19 14:47:15 -0300

    - Update: service worker;

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { registerServiceWorker } from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+serviceWorker.register();
